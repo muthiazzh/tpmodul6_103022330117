8e0dbcb956188ab96113c32a60d1d802766ec822
muthiazzh <muthiahazzahra200@gmail.com>
2025-03-23 22:10:27 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022330117/Program.cs
+++ tpmodul6_103022330117/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using tpmodul6_103022330117;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Muthi'ah Az Zahra");
+        video.PrintVideoDetails();
+
+        video.IncreasePlayCount(404);
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_103022330117/SayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022330117
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+                throw new ArgumentException("Title cannot be null or empty");
+
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999); 
+            this.title = title;
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0)
+                throw new ArgumentException("Play count cannot be negative");
+
+            this.playCount += count;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
+
